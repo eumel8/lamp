605a61ac8dd30745704eb4242d380ca8cb047b38
Frank Kloeker <f.kloeker@telekom.de>
2015-06-27 20:49:45 +0200

    improve build script for deb + rpm

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ DATE=`date +%s`
 
 # fpm -s dir -t deb -C /tmp/project --name project_name --version 1.0.0 --iteration 1 --depends debian_dependency1 --description "A sample package" .
 
+if [ `which fpm` ] 
+then
 fpm -s dir -t deb -d 'puppet' -v "01-$BRANCH-$DATE"  \
         -n lamp-conf  \
 	--iteration 1 \
@@ -13,12 +15,30 @@ fpm -s dir -t deb -d 'puppet' -v "01-$BRANCH-$DATE"  \
         --description "LAMP Configuration package" \
         --prefix /etc/puppet  .
 
+if [ -f "lamp-conf_01-${BRANCH}-${DATE}-1_amd64.deb" ] 
+then
+sudo mv "lamp-conf_01-${BRANCH}-${DATE}-1_amd64.deb" /build/www/
+sudo rm -f "/build/www/lamp-conf_01-${BRANCH}_latest-1_amd64.deb" 
+sudo ln -s "/build/www/lamp-conf_01-${BRANCH}-${DATE}-1_amd64.deb" "/build/www/lamp-conf_01-${BRANCH}_latest-1_amd64.deb" 
+fi
 
-fpm -s dir -t rpm -d 'puppet' -v "$BRANCH-$DATE"  \
+
+fpm -s dir -t rpm -d 'puppet' -v "$BRANCH"_"$DATE"  \
         -n lamp-conf -a all \
         -x .git -x .gitmdules  \
         --rpm-auto-add-directories \
         --replaces lamp-conf \
         --description "LAMP Configuration package" \
         --prefix /etc/puppet  .
+if [ -f "lamp-conf-${BRANCH}"_"${DATE}-1.noarch.rpm" ] 
+then
+sudo mv "lamp-conf-${BRANCH}"_"${DATE}-1.noarch.rpm" /build/www/
+sudo rm -f "/build/www/lamp-conf-${BRANCH}_latest-1.noarch.rpm" 
+sudo ln -s "/build/www/lamp-conf-${BRANCH}"_"${DATE}-1.noarch.rpm" "/build/www/lamp-conf-${BRANCH}_latest-1.noarch.rpm" 
+fi
+
+else 
+echo "fpm is needed...."
+fi
+
 
